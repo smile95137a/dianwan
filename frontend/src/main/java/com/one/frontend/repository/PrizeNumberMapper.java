a764a3a904136bb0cdba9c7ab5730b62c7c03d6f
Jimmy <a0970124936@gmail.com>
2024-08-09 14:09:08 +0800

    update

--- frontend/src/main/java/com/one/frontend/repository/PrizeNumberMapper.java
+++ frontend/src/main/java/com/one/frontend/repository/PrizeNumberMapper.java
@@ -11,14 +11,14 @@ import java.util.List;
 public interface PrizeNumberMapper {
 
     // 获取指定产品明细的所有奖品编号
-    @Select("SELECT * FROM PrizeNumber WHERE product_detail_id = #{productDetailId}")
+    @Select("SELECT * FROM prizenumber WHERE product_detail_id = #{productDetailId}")
     List<PrizeNumber> getAllPrizeNumbersByProductDetailId(Long productDetailId);
 
     // 获取指定产品明细中未被抽走的奖品编号
-    @Select("SELECT * FROM PrizeNumber WHERE product_id = #{productDetailId} AND is_drawn = FALSE")
+    @Select("SELECT * FROM prizenumber WHERE product_id = #{productDetailId} AND is_drawn = FALSE")
     List<PrizeNumber> getAvailablePrizeNumbersByProductDetailId(Long productDetailId);
 
     // 标记奖品编号为已抽走
-    @Update("UPDATE PrizeNumber SET is_drawn = TRUE WHERE prize_number_id = #{prizeNumberId} and product_id = #{productId} and product_detail_id = #{productDetailId}")
+    @Update("UPDATE prizenumber SET is_drawn = TRUE WHERE prize_number_id = #{prizeNumberId} and product_id = #{productId} and product_detail_id = #{productDetailId}")
     void markPrizeNumberAsDrawn(Long prizeNumberId , Long productId , Long productDetailId);
 }
