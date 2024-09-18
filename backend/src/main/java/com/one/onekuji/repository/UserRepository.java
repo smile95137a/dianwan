1567fd017d92bf1d7c29e32956e15d373b31ad09
Jimmy <a0970124936@gmail.com>
2024-09-18 17:24:35 +0800

    no message

--- backend/src/main/java/com/one/onekuji/repository/UserRepository.java
+++ backend/src/main/java/com/one/onekuji/repository/UserRepository.java
@@ -10,23 +10,23 @@ import java.util.List;
 @Mapper
 public interface UserRepository {
 
-    @Select("SELECT id, username, password, nick_name, email, phone_number, address, role_id, balance, bonus, sliver_coin, google_id, updated_at, draw_count FROM users")
+    @Select("SELECT id, username, password, nick_name, email, phone_number, address, role_id, balance, bonus, sliver_coin, google_id, updated_at, draw_count FROM user")
     List<UserRes> findAll();
 
-    @Select("SELECT id, username, password, nick_name, email, phone_number, address, role_id, balance, bonus, sliver_coin, google_id, updated_at, draw_count FROM users WHERE id = #{userId}")
+    @Select("SELECT id, username, password, nick_name, email, phone_number, address, role_id, balance, bonus, sliver_coin, google_id, updated_at, draw_count FROM user WHERE id = #{userId}")
     UserRes findById(Long userId);
 
-    @Insert("INSERT INTO users (username, password, nick_name, email, phone_number, address, role_id, balance, bonus, sliver_coin, google_id, created_at, draw_count , role_id) " +
+    @Insert("INSERT INTO user (username, password, nick_name, email, phone_number, address, role_id, balance, bonus, sliver_coin, google_id, created_at, draw_count , role_id) " +
             "VALUES (#{username}, #{password}, #{nickName}, #{email}, #{phoneNumber}, #{address}, #{roleId}, #{balance}, #{bonus}, #{sliverCoin}, #{googleId}, #{createdAt}, #{drawCount}) , #{roleId}")
     @Options(useGeneratedKeys = true, keyProperty = "userId", keyColumn = "id")
     void insert(UserReq userReq);
 
-    @Update("UPDATE users SET username = #{username}, password = #{password}, nick_name = #{nickName}, email = #{email}, phone_number = #{phoneNumber}, address = #{address}, " +
+    @Update("UPDATE user SET username = #{username}, password = #{password}, nick_name = #{nickName}, email = #{email}, phone_number = #{phoneNumber}, address = #{address}, " +
             "role_id = #{roleId}, balance = #{balance}, bonus = #{bonus}, sliver_coin = #{sliverCoin}, google_id = #{googleId}, updated_at = NOW(), draw_count = #{drawCount} " +
             "WHERE id = #{userId}")
     void update(UserReq userReq);
 
-    @Delete("DELETE FROM users WHERE id = #{userId}")
+    @Delete("DELETE FROM user WHERE id = #{userId}")
     void delete(Long userId);
 
     @Select("select * from user where username = #{username}")
