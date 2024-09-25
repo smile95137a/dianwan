be0c82b1686d59627d0e05de966f5c9fa1c52ddd
Jimmy <a0970124936@gmail.com>
2024-09-26 03:43:39 +0800

    no message

--- backend/src/main/java/com/one/onekuji/config/SpringSecurityConfig.java
+++ backend/src/main/java/com/one/onekuji/config/SpringSecurityConfig.java
@@ -11,7 +11,6 @@ import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -57,18 +56,15 @@ public class SpringSecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf(AbstractHttpConfigurer::disable)
-                .cors(cors -> cors.and()) // 确保 CORS 配置生效
+        http.csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .exceptionHandling(exception -> exception
                         .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)))
-                .authorizeHttpRequests(auth ->
-                        auth.requestMatchers("/auth/**", "/oauth2/**").permitAll()
-                                .requestMatchers("/img/**").permitAll()
-                                .anyRequest().authenticated()
-                );
-
+                .authorizeHttpRequests(
+                        auth -> auth.requestMatchers("/api/auth/**", "/oauth2/**").permitAll().requestMatchers("/img/**")
+                                .permitAll().requestMatchers("/**").permitAll().anyRequest().authenticated());
         http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+
         return http.build();
     }
 
