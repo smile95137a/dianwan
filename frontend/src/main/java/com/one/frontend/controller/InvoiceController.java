6d489354ca2bfeea61f1162b4e91b5efdc1ce175
Jimmy <a0970124936@gmail.com>
2024-09-27 15:03:46 +0800

    no message

--- frontend/src/main/java/com/one/frontend/controller/InvoiceController.java
+++ frontend/src/main/java/com/one/frontend/controller/InvoiceController.java
@@ -1,7 +1,5 @@
 package com.one.frontend.controller;
 
-import com.one.frontend.config.security.CustomUserDetails;
-import com.one.frontend.config.security.SecurityUtils;
 import com.one.frontend.request.ReceiptReq;
 import com.one.frontend.response.ReceiptRes;
 import com.one.frontend.service.InvoiceService;
@@ -22,8 +20,8 @@ public class InvoiceController {
 
     @PostMapping("/add")
     public void addInvoice(@RequestBody ReceiptReq invoiceRequest) throws MessagingException {
-        CustomUserDetails userDetails = SecurityUtils.getCurrentUserPrinciple();
-        Long userId = userDetails.getId();
+//        CustomUserDetails userDetails = SecurityUtils.getCurrentUserPrinciple();
+//        Long userId = userDetails.getId();
         ResponseEntity<ReceiptRes> res = invoiceService.addB2CInvoice(invoiceRequest);
         System.out.println(res.getBody());
 //        ReceiptRes receiptRes = res.getBody();
