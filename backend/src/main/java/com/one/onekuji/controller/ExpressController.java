7920d8337c38e38274d52d5f65749fbea4650b63
Jimmy <a0970124936@gmail.com>
2024-10-08 02:04:08 +0800

    no message

---
+++ backend/src/main/java/com/one/onekuji/controller/ExpressController.java
@@ -0,0 +1,55 @@
+package com.one.onekuji.controller;
+
+import org.springframework.http.*;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class ExpressController {
+
+    @PostMapping("/logistics/callback")
+    public ResponseEntity<String> logisticsCallback(
+            @RequestParam String storename,
+            @RequestParam String storied
+    ) {
+        // 處理回傳的店名和店號
+        System.out.println("Store Name: " + storename);
+        System.out.println("Store ID: " + storied);
+
+        // 根據業務邏輯處理
+        return ResponseEntity.ok("Received logistics info successfully");
+    }
+
+
+    @GetMapping("/test")
+    public ResponseEntity<String> logisticsCallback() {
+        String url = "https://logistics.gomypay.asia/Logisticstm.aspx";
+
+        // 設定 POST 的參數
+        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+        params.add("Url", "https://onemorelottery.tw/logistics/callback");
+        params.add("Opmode", "3");
+
+        // 設定 Headers
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED); // 設定為表單格式
+
+        // 封裝請求
+        HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(params, headers);
+
+        // 發送 POST 請求
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.POST, request, String.class);
+
+        // 處理回應
+        System.out.println("Response: " + response.getBody());
+        // 根據業務邏輯處理
+        return ResponseEntity.ok("Received logistics info successfully");
+    }
+
+}
---
+++ backend/src/main/java/com/one/onekuji/model/Express.java
@@ -0,0 +1,15 @@
+package com.one.onekuji.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Express {
+
+    private String storename; //711店名
+
+    private String storied; //711店號
+}
