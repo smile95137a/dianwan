83931b428ab6898b8f67f48355d545a3dd501d7a
Jimmy <a0970124936@gmail.com>
2024-10-07 18:27:11 +0800

    no message

--- backend/src/main/java/com/one/onekuji/repository/UserRepository.java
+++ backend/src/main/java/com/one/onekuji/repository/UserRepository.java
@@ -13,17 +13,17 @@ public interface UserRepository {
     @Select("SELECT id, username, password, nickname, email, phone_number, address, role_id, balance, bonus, sliver_coin, updated_at, draw_count FROM user")
     List<UserRes> findAll();
 
-    @Select("SELECT id, username, password, nick_name, email, phone_number, address, role_id, balance, bonus, sliver_coin,, updated_at, draw_count FROM user WHERE id = #{userId}")
+    @Select("SELECT id, username, password, nickname, email, phone_number, address, role_id, balance, bonus, sliver_coin,, updated_at, draw_count FROM user WHERE id = #{userId}")
     UserRes findById(Long userId);
 
     // 插入新用戶
-    @Insert("INSERT INTO user (username, password, nick_name, email, phone_number, address, role_id, balance, created_at) " +
+    @Insert("INSERT INTO user (username, password, nickname, email, phone_number, address, role_id, balance, created_at) " +
             "VALUES (#{username}, #{password}, #{nickname}, #{email}, #{phoneNumber}, #{address}, #{roleId}, #{balance}, #{createdAt})")
     @Options(useGeneratedKeys = true, keyProperty = "id", keyColumn = "id")
     void insert(User user);
 
     // 更新用戶
-    @Update("UPDATE user SET username = #{username}, password = #{password}, nick_name = #{nickname}, email = #{email}, phone_number = #{phoneNumber}, address = #{address}, " +
+    @Update("UPDATE user SET username = #{username}, password = #{password}, nickname = #{nickname}, email = #{email}, phone_number = #{phoneNumber}, address = #{address}, " +
             "role_id = #{roleId}, balance = #{balance}, updated_at = #{updatedAt} " +
             "WHERE id = #{id}")
     void update(User user);
